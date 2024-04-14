c80397ba2604eb7b64e13f274e27d5933cad600a
Anjin Lee <anjin720jin@naver.com>
2024-04-14 18:31:04 +0900

    first commit

---
+++ Diory/DioryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DioryApp.swift
+//  Diory
+//
+//  Created by Anjin on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DioryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
